4403c9cd6ff6b2c8cd2739d7fc9af56384560851
Justin Davidson <jutstindavidson23@gmail.com>
2016-08-10 15:31:58 +1000

    Express App talking to Rethinkdb

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-var express = require('express');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var helmet = require('helmet');
-require('dotenv').config({silent: true});
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+require('dotenv').config({silent: true})
 
-var app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(helmet());
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(helmet())
 
 // --------------------------------------------- Routes
 
-var users = require('./routes/users');
-var login = require('./routes/login');
+const users = require('./routes/users')
+const login = require('./routes/login')
+const test = require('./routes/test')
 
-app.use('/users', users);
-app.use('/login', login);
+app.use('/users', users)
+app.use('/login', login)
+app.use('/test', test)
 
 app.get('/', function(request, response) {
-  response.send("Heyyy");
-});
+  response.send("Heyyy")
+})
+
+
 
 // --------------------------------------------- Routes
 
-app.use(function(error, request, response, next) {
-  response.status(error.status || 500);
-  response.json({ error: error.message });
-});
+app.use((error, request, response, next) => {
+  response.status(error.status || 500)
+  response.json({ error: error.message })
+})
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(3000, () => {
+  const host = server.address().address
+  const port = server.address().port
 
-  console.log('App listening on 3000');
+  console.log(`App listening on ${host}/${port}`)
 })
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // GET request to /login
 router.get('/', function(req, res, next) {
-    res.send("This is the users page");
-});
+    res.send("This is the users page")
+})
 
 // POST request to /login
 router.post('/', function(req, res, next) {
     // do something
-});
+})
 
-module.exports = router;
+module.exports = router
